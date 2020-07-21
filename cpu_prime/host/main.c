d8069c4ba71e64f79a3c58937d70bdc4e296c8cd
HaoXuan-a <55918769+HaoXuan-a@users.noreply.github.com>
2020-07-21 17:57:59 +0800

    Update main.c

--- cpu_prime/host/main.c
+++ cpu_prime/host/main.c
@@ -223,7 +223,9 @@ int main(void)
 	printf("-----------------------------time: %.4f s\n",timeuse) ;
 
 	/*
-	 *完成了TA调用，关闭会话并结束上下文。
+	 *完成与TA交互后，关闭会话并结束上下文。
+	 *
+	 *当会话关闭后，TA会在日志中打印"Goodbye!" 
 	 */
 
 	TEEC_CloseSession(&sess);
